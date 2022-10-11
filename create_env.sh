8f07e1512bbcf03f53ea2dbceb47a3d73115078d
carlos <carlos.gutierrezalva@uah.es>
2022-10-11 13:43:01 +0200

    deleted cmake from environment

--- create_env.sh
+++ create_env.sh
@@ -4,7 +4,7 @@ export CONDA_ALWAYS_YES="true"
 
 # Create conda env
 source ~/miniconda3/etc/profile.d/conda.sh
-conda create -n pyril-headless python=3.7.0 cmake=3.14.0
+conda create -n pyril-headless python=3.7.0
 conda activate pyril-headless
 
 # Install habitat sim
